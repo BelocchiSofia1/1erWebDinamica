79e3f26f430830721de3ceb94fea6778e68b6014
Sofia Belocchi <sofiabelocchiet@gmail.com>
2023-05-05 00:05:43 -0300

    Primer Web Dinamica

---
+++ index.js
@@ -0,0 +1,27 @@
+import datos from "./personas.json" assert {type: "json"}
+//aseert para no tener que Parsear el Json
+
+window.addEventListener("load", () => {
+
+// Obtener el contenedor de las cards (todas las tarj)
+const container = document.getElementById('cards-container');
+
+// Obtener la plantilla de la card (cada una)
+const cardTemplate = document.getElementById('card-template');
+
+  // Iterar sobre los datos y crear una card para cada uno
+  datos.forEach(dato => {
+
+    // Clonar la plantilla de la card
+    const card = cardTemplate.content.cloneNode(true);
+
+    // Agregar los datos a la card
+    card.querySelector('.card-title').textContent = dato.nombre;
+    card.querySelector('.card-apellido').textContent = dato.apellido;
+    card.querySelector('.card-edad').textContent = dato.edad;
+    card.querySelector('.card-mail').textContent = dato.email;
+    
+    // Agregar la card al contenedor
+    container.appendChild(card);
+  });
+})
